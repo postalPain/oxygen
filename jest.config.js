c2aa9c229bc30f2a80247aa2943c1164ced400e9
AlinaKuzmenko <aline.kuzmenko@gmail.com>
2021-09-21 11:32:25 +0300

    add detox setup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'react-native',
   testPathIgnorePatterns: ['<rootDir>/e2e'],
-  setupFiles: ['<rootDir>/setupTests.js']
+  setupFiles: ['<rootDir>/setupTests.js'],
 };
--- setupTests.js
+++ setupTests.js
@@ -1,7 +1,15 @@
 import { NativeModules } from 'react-native';
+import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
 
 NativeModules.SettingsManager = {
   settings: {
     AppleLocale: 'en',
   },
+};
+NativeModules.RNCNetInfo = {
+  getCurrentState: jest.fn(() => Promise.resolve()),
+  addListener: jest.fn(),
+  removeListeners: jest.fn(),
 };
