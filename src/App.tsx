61ec0e5977ba49449d6df4b216701b521856f498
Sergiy Ostrovsky <sergiy.ostrovsky@stryber.com>
2021-10-04 16:08:43 +0300

    Add react-native splash screen handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { ThemeContextProvider } from '@stryberventures/stryber-react-native-ui-components';
 import store from 'modules/store';
 import Navigation from 'navigation';
 import theme from 'config/theme';
+import SplashScreen from 'react-native-splash-screen';
 
 // import { setupSentry } from 'services/Sentry/sentry';
 
 export const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider store={store}>
       <ThemeContextProvider themes={[theme]}>
@@ -15,5 +19,5 @@ export const App = () => {
       </ThemeContextProvider>
     </Provider>
   );
-}
+};
 export default App;
