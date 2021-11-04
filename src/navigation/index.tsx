1cb044b0fd601b74c9c9e539639e34a7db335588
Sergiy Ostrovsky <sergiy.ostrovsky@stryber.com>
2021-11-04 21:15:49 +0200

    Remove <Loading />

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import {
   DataPrivacy,
   EnterEmail,
@@ -23,7 +22,12 @@ import { AppScreenNames } from './types';
 import { IconBack, NavigationHeader, } from 'components';
 import theme from 'config/theme';
 import { headerStyles } from './styles';
-import Loading from 'screens/Loading';
+import { AuthStoredKeys } from 'modules/auth/types';
+import { getMultipleItems } from 'modules/asyncStorage';
+import { useDispatch } from 'react-redux';
+import { setAuthData } from 'modules/auth/actions';
+import { checkVerification } from 'modules/user/actions';
+import { isPending } from 'modules/user/selectors';
 
 const AppStack = createNativeStackNavigator();
 
@@ -40,13 +44,44 @@ const getHeaderOptions = () => ({
 });
 
 const Navigation = () => {
+  const dispatch = useDispatch();
+  const navigationRef = React.useRef(null);
+  const navigate = (name: any, params?: any) => {
+    if (navigationRef && navigationRef.current) {
+      navigationRef?.current?.navigate(name, params);
+    }
+  };
+
+  useEffect(
+    () => {
+      getMultipleItems([
+        AuthStoredKeys.access_token,
+        AuthStoredKeys.access_ttl,
+        AuthStoredKeys.refresh_token,
+        AuthStoredKeys.refresh_ttl,
+        AuthStoredKeys.email,
+      ]).then((_authData) => {
+        dispatch(setAuthData(_authData));
+        dispatch(checkVerification({
+          onSuccess: (status) => {
+            if (isPending(status)) {
+              navigate(AppScreenNames.UserVerificationPending);
+            } else {
+              navigate(AppScreenNames.SignIn);
+            }
+          },
+          onError: () => {
+            navigate(AppScreenNames.Onboarding);
+          }
+        }));
+      });
+    },
+    []
+  );
+
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <AppStack.Navigator>
-        <AppStack.Screen
-          name={AppScreenNames.Loading}
-          component={Loading}
-        />
         <AppStack.Screen
           name={AppScreenNames.Onboarding}
           component={Onboarding}
