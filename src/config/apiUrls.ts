382d8e433294a093431159e066b3fdbb636c4c0f
AlinaKuzmenko <aline.kuzmenko@gmail.com>
2021-12-07 16:55:36 +0200

    use variables from node env

--- src/config/apiUrls.ts
+++ src/config/apiUrls.ts
@@ -1,15 +1,10 @@
-export const DEV_URL = 'https://api-dev.stryproject-o.ch/api/v1';
-export const STAGE_URL = 'https://api-stage.stryproject-o.ch/api/v1';
-export const PROD_URL = 'https://api-prod.stryproject-o.ch/api/v1';
-export const BASE_URL = DEV_URL;
-
 export default {
-  signUp: 'employees/sign-up',
-  checkVerification: 'employees/verification/status',
-  resendVerificationCode: 'employees/verification/resend-email',
-  verifyEmail: 'employees/verification/email',
-  refreshToken: 'refresh',
-  userInfo: 'employees/me',
-  getTransactions: 'employees/transactions',
-  signOut: 'logout',
+  signUp: '/employees/sign-up',
+  checkVerification: '/employees/verification/status',
+  resendVerificationCode: '/employees/verification/resend-email',
+  verifyEmail: '/employees/verification/email',
+  refreshToken: '/refresh',
+  userInfo: '/employees/me',
+  getTransactions: '/employees/transactions',
+  signOut: '/logout',
 };
