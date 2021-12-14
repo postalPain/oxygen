ceee8be6c3ec518eaf2e552e24b44cc47e9cb199
Sergiy Ostrovsky <sergiy.ostrovsky@stryber.com>
2021-12-14 15:31:01 +0200

    Add debug

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -14,6 +14,7 @@ import { existsInStoredLoginEmails } from 'modules/user/asyncStorage';
 import { checkVerification, userGetInfo } from 'modules/user/actions';
 import { errorNotification } from 'modules/notifications/actions';
 import { VerificationStatuses } from 'modules/user/types';
+import DebugView from 'components/DebugView';
 
 const SignIn = (
   { navigation }: AppNavigationProps<AppScreenNames.SignIn>
@@ -111,6 +112,9 @@ const SignIn = (
           </Link>
         </View>
       </View>
+      { password === 'debug debugovich' && (
+        <DebugView />
+      )}
       <View style={styles.buttonSection}>
         <Button
           onPress={() => {
