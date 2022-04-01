b9f6510a7942d2f716686dc08455e80c0ea40ea5
Sergiy Ostrovsky <sergiy.ostrovsky@stryber.com>
2022-04-01 15:51:02 +0300

    Use access token from store, because axios header set to null on hot reload

--- src/services/api/request.ts
+++ src/services/api/request.ts
@@ -9,6 +9,7 @@ import { isTtlActive } from 'utils/time';
 import api from '.';
 import { handleBackendError } from './errors';
 import env from 'env';
+import { getLogger } from 'modules/logger';
 
 
 const request = axios.create({
@@ -36,14 +37,11 @@ export const removeHeader = (headerName: string, callback?: () => void) => {
   if (callback) callback();
 };
 
-export const setAuthHeader = (access_token) => addHeader({
-  name: 'Authorization',
-  value: `Bearer ${access_token}`,
-});
-
 request.interceptors.request.use(
   async (config) => {
     const authData = selectAuthData(getState());
+    config.headers.Authorization = `Bearer ${authData.access_token}`;
+
     if ((config.url !== apiUrls.refreshToken) && authData.access_token && !isTtlActive(authData.access_ttl)) {
       if (isTtlActive(authData.refresh_ttl)) {
         const response = await api.auth.refreshToken({
