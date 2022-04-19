6be5d5e42dd726d51893226ea3503f4082094294
Sergiy Ostrovsky <sergiy.ostrovsky@stryber.com>
2022-04-19 16:07:07 +0300

    Fix push handling on the background

---
+++ src/modules/pushNotifications/hooks/usePushMessages.ts
@@ -0,0 +1,34 @@
+import messaging, { FirebaseMessagingTypes } from '@react-native-firebase/messaging';
+import useLogger from 'modules/logger/hooks/useLogger';
+import { useEffect, useState } from 'react';
+import { uuid } from 'utils/uuid';
+
+export const usePushMessages = <T>(topic?: string) => {
+  type TMessage = FirebaseMessagingTypes.RemoteMessage & {data: T};
+  const [message, setMessage] = useState<TMessage>(null);
+
+  const logger = useLogger();
+  const onMessage = (_message: TMessage): any => {
+    logger.log('topic', uuid(), topic);
+    logger.log('_message', _message);
+    if (!topic || (topic === _message.data.topic)) {
+      logger.log('setting message');
+      setMessage(_message);
+    }
+  };
+  useEffect(() => {
+    const unsubscribeMessage = messaging().onMessage(onMessage);
+    const unsubscribeNotificationOpenedApp = messaging().onNotificationOpenedApp(onMessage);
+    return () => {
+      unsubscribeMessage();
+      unsubscribeNotificationOpenedApp();
+    };
+  }, []);
+
+  return {
+    simulateMessage: (_message: TMessage) => {
+      onMessage(_message);
+    },
+    message,
+  };
+};
