9d47939a638a02d671043dd2e40768dad86b0cd9
Sergiy Ostrovsky <sergiy.ostrovsky@stryber.com>
2022-04-20 14:09:38 +0300

    Remove logging

--- src/modules/pushNotifications/hooks/usePushMessages.ts
+++ src/modules/pushNotifications/hooks/usePushMessages.ts
@@ -1,18 +1,12 @@
 import messaging, { FirebaseMessagingTypes } from '@react-native-firebase/messaging';
-import useLogger from 'modules/logger/hooks/useLogger';
 import { useEffect, useState } from 'react';
-import { uuid } from 'utils/uuid';
 
 export const usePushMessages = <T>(topic?: string) => {
   type TMessage = FirebaseMessagingTypes.RemoteMessage & {data: T};
   const [message, setMessage] = useState<TMessage>(null);
 
-  const logger = useLogger();
   const onMessage = (_message: TMessage): any => {
-    logger.log('topic', uuid(), topic);
-    logger.log('_message', _message);
     if (!topic || (topic === _message.data.topic)) {
-      logger.log('setting message');
       setMessage(_message);
     }
   };
