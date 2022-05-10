f4e92e19bde224d30adcf1e70fb1bd4abd7b9200
Sergiy O <sergiy.ostrovsky@stryber.com>
2022-05-10 22:55:09 +0300

    Changes to useDatabase

--- src/modules/fbDatabase/useDatabase.ts
+++ src/modules/fbDatabase/useDatabase.ts
@@ -1,30 +1,26 @@
 import { useEffect, useState } from 'react';
 import { firebase } from '@react-native-firebase/database';
 
-export enum DbKeys {
-  minSupportedBuild = '/force_update/build_no',
-};
-
-export const useDatabase = <T>(key?: DbKeys) => {
-  const [dbValue, setDbValue] = useState<T>(null);
+export const useDatabase = <T>(key?: string) => {
+  const [value, setValue] = useState<T>(null);
 
   useEffect(() => {
     fetchDbValue(key);
   }, []);
 
-  const fetchDbValue = async<T2 = T> (_key?: DbKeys) => {
+  const fetchDbValue = async<T2 = T> (_key?: string) => {
     const ref = firebase
       .app()
       .database('https://floos-e4dc6-default-rtdb.europe-west1.firebasedatabase.app/')
       .ref(_key);
 
-    const value = (await ref.once('value')).val();
-    setDbValue(value);
-    return value as T2;
+    const _value = (await ref.once('value')).val();
+    setValue(_value);
+    return _value as T2;
   };
 
   return {
-    dbValue,
+    value,
     fetchDbValue,
   };
 };
