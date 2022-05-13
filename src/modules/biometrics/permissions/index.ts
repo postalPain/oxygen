7ecc82a5ed6a6c33fcff1aa6d5bb5e21251379e5
Sergiy O <sergiy.ostrovsky@stryber.com>
2022-05-13 18:56:36 +0300

    Remove request for permission for TouchID

--- src/modules/biometrics/permissions/index.ts
+++ src/modules/biometrics/permissions/index.ts
@@ -1,8 +1,13 @@
 import env from 'env';
 import { request, PERMISSIONS } from 'react-native-permissions';
+import { getDeviceBiometryStatus } from '../biometrics';
 
 export const requestBiometricPermission = async () => {
   if (env.ios) {
+    const biometryStatus = await getDeviceBiometryStatus();
+    if (biometryStatus.biometryType === 'TouchID') {
+      return true;
+    }
     const result = await request(PERMISSIONS.IOS.FACE_ID);
     return result === 'granted';
   }
