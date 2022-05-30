e9a042a199e9d33850ccaec7ea202bd69f91a6cf
Sergiy O <sergiy.ostrovsky@stryber.com>
2022-05-30 15:51:36 +0300

    Test Crashlytics

--- src/components/DebugView/index.tsx
+++ src/components/DebugView/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { ScrollView } from 'react-native-gesture-handler';
 import DebugAsyncStorage from './DebugAsyncStorage';
@@ -7,12 +7,19 @@ import SettingsBiometrics from 'components/SettingsBiometrics';
 import DebugLogs from './DebugLogs';
 import useAskForReview from 'modules/askForReview/hooks/useAskForReview';
 import Link from 'components/Link';
+import crashlytics from '@react-native-firebase/crashlytics';
+
 
 const DebugView = () => {
   const askForReview = useAskForReview();
 
   return (
     <ScrollView>
+      <Link onPress={() => {
+        crashlytics().crash();
+      }}
+      >Crash
+      </Link>
       <DebugPush />
       <DebugAsyncStorage />
       <SettingsBiometrics />
