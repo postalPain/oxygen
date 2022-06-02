3424ce766785d1a975b3cb1ec49cd42d118e1695
Sergiy O <sergiy.ostrovsky@stryber.com>
2022-06-02 17:19:30 +0300

    Fix crashlytics for Android

--- src/components/DebugView/index.tsx
+++ src/components/DebugView/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { ScrollView } from 'react-native-gesture-handler';
 import DebugAsyncStorage from './DebugAsyncStorage';
